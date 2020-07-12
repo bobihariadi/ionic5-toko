57ec689414bc560e0683dde66cf2538961f10efd
bobihariadi <bobihariadi@gmial.com>
2020-07-12 15:46:01 +0700

    config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 
-export const api_base_url = "http://localhost/ci-restjwt/";
-// export const api_base_url = "http://103.29.187.169/ci-restjwt/";
+// export const api_base_url = "http://localhost/ci-restjwt/";
+export const api_base_url = "http://103.29.187.169/ci-restjwt/";
